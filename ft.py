81d477c4e1f24fee4fc7ae125cb9f84c128d98a3
Abdullah <abdullah240804@gmail.com>
2026-03-03 15:40:58 +0530

    adding ft.py file

---
+++ ft.py
@@ -0,0 +1,21 @@
+# length =float(input("Enter the value of Length:"))
+# breath =float(input("Enter the value of Breath:"))
+# result=length * breath
+# print(f"Area of rectangle is {result}")
+
+# name=input("Enter your name:")
+# print(f"Hello {name}")
+import math
+# radius=float(input("Enter the value of Radius: "))
+# cir = float(2 * math.pi * radius)
+# result = round(cir)
+
+# print (f"The value of Circumference: {result})
+
+Radius =float(input("Enter the value of Radius:"))
+Area = math.pi * pow(Radius , 2)
+result = round (Area)
+print(f"The area of circle is {Area}")
+print(f"The area of circle is {result}")
+
+
