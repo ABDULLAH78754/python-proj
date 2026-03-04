d4a838a135058a6181683e742e20b4417351d458
Abdullah <abdullah240804@gmail.com>
2026-03-04 21:45:27 +0530

    Adding modification

--- calculator.full.py
+++ calculator.full.py
@@ -5,19 +5,22 @@ def calculator():
     while True:
          value=float(input("Enter the value of input: "))
 
-         operator = input("Enter an operator (+ - * / %): ") 
+          
          if value == 0:
                 break
-         elif operator =="+":
-                result += value
-         elif operator =="-":
+         else:
+              operator = input("Enter an operator (+ - * / %): ")
+              if operator =="+":
+                  result += value
+              elif operator =="-":
                 result -= value
-         elif operator =="*":
+              elif operator =="*":
                 result *= value
-         elif operator =="/":
+              elif operator =="/":
                 result /= value 
-         elif operator =="%":
+              elif operator =="%":
                 result %= value
+         
     print(result)
 
 calculator()
